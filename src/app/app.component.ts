e5ae3bebfacaeb679d6c5795fdfaf0fe9bb9220c
DanielAndresSmg <danielandressmg@gmail.com>
2023-06-30 17:21:28 +0700

    update portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Employee } from './models/employee.model';
+import { EmployeeService } from './services/employee.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit, OnInit {
+  @ViewChild('fileInput') fileInput: any;
   title = 'Employee-Management';
+
+  employeeForm: FormGroup;
+  employees: Employee[];
+  employeesToDisplay: Employee[];
+  educationOptions = [
+    '10th pass',
+    'diploma',
+    'graduation',
+    'post graduation',
+    'Phd',
+  ];
+
+  constructor(
+    private fb: FormBuilder,
+    private employeeService: EmployeeService
+  ) {
+    this.employeeForm = fb.group({});
+    this.employees = [];
+    this.employeesToDisplay = this.employees;
+    this.
+  }
+
+
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      firstname: this.fb.control(''),
+      lastname: this.fb.control(''),
+      birtday: this.fb.control(''),
+      gender: this.fb.control(''),
+      education: this.fb.control('default'),
+      company: this.fb.control(''),
+      jobExperience: this.fb.control(''),
+      salary: this.fb.control(''),
+    });
+    this.employeeService.getEmployees().subscribe((res) => {
+      for()
+    });
+  }
+
+  ngAfterViewInit(): void {
+    // this.buttontemp.nativeElement.click();
+  }
+
+  clearForm() {
+    this.FirstName.setValue('');
+    this.LastName.setValue('');
+    this.Birthday.setValue('');
+    this.Gender.setValue('');
+    this.Education.setValue('');
+    this.Company.setValue('');
+    this.JobExperience.setValue('');
+    this.Salary.setValue('');
+    this.fileInput.nativeElement.value = '';
+  }
+
+  addEmployee() {
+    let employee: Employee = {
+      firstName: this.FirstName.value,
+      lastName: this.LastName.value,
+      birthdate: this.Birthday.value,
+      gender: this.Gender.value,
+      education: this.Education.value,
+      company: this.Company.value,
+      jobExperience: this.JobExperience.value,
+      salary: this.Salary.value,
+      profile: this.fileInput.nativeElement.file[0]?.name,
+    };
+    this.employeeService.postEmployee(employee).subscribe((res) => {
+      this.employees.unshift(res);
+      this.clearForm();
+    });
+  }
+
+  public get FirstName(): FormControl {
+    return this.employeeForm.get('firstname') as FormControl;
+  }
+
+  public get LastName(): FormControl {
+    return this.employeeForm.get('lastname') as FormControl;
+  }
+
+  public get Birthday(): FormControl {
+    return this.employeeForm.get('birthday') as FormControl;
+  }
+
+  public get gender(): FormControl {
+    return this.employeeForm.get('gender') as FormControl;
+  }
+
+  public get Education(): FormControl {
+    return this.employeeForm.get('education') as FormControl;
+  }
+
+  public get Company(): FormControl {
+    return this.employeeForm.get('company') as FormControl;
+  }
+
+  public get JobExperience(): FormControl {
+    return this.employeeForm.get('jobExperience') as FormControl;
+  }
+
+  public get Salary(): FormControl {
+    return this.employeeForm.get('salary') as FormControl;
+  }
 }
